b5b05b70476963d07bf7f3f92399647b8b437411
thiagocontelli <thiagocontellid@gmail.com>
2023-02-22 14:23:56 -0300

    feat: add a grid of cards in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Card } from "./components/Card";
-import { Header } from "./components/Header";
-import { defaultTheme } from "./themes/default";
+import { Container, CssBaseline, Grid, ThemeProvider } from '@mui/material';
+import { Card } from './components/Card';
+import { Header } from './components/Header';
+import { shuffledCards } from './mocks/cards';
+import { defaultTheme } from './themes/default';
 
-export function App () {
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Header />
-      <Card />
+      <Container>
+        <Grid rowSpacing={4} columnSpacing={4} container>
+          {shuffledCards.map(card => (
+            <Grid key={card.id} item>
+              <Card image={card.image} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </ThemeProvider>
-  )
+  );
 }
