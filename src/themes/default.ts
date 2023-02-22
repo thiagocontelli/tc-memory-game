c6e81f40c7305d921d81a7add853ad7543e24516
thiagocontelli <thiagocontellid@gmail.com>
2023-02-22 20:20:59 -0300

    refactor: change primary color

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -2,7 +2,10 @@ import { createTheme } from "@mui/material";
 
 export const defaultTheme = createTheme({
   palette: {
-    mode: 'dark'
+    mode: 'dark',
+    primary: {
+      main: '#358C6C'
+    }
   },
   typography: {
     fontFamily: 'Poppins'
